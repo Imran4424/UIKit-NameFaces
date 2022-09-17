f58ac173e829e6cf8a2c62db87f8d0c1b364ee10
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2022-09-18 04:47:56 +0600

    design collection view

---
+++ NameFaces/PersonCell.swift
@@ -0,0 +1,13 @@
+//
+//  PersonCell.swift
+//  NameFaces
+//
+//  Created by Shah Md Imran Hossain on 18/9/22.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet weak var personImage: UIImageView!
+    @IBOutlet weak var personName: UILabel!
+}
---
+++ NameFaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NameFaces
+//
+//  Created by Shah Md Imran Hossain on 17/9/22.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
