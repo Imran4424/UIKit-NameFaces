62408139a510a3cb00fa4f7e3ad5d7fb9ae9233c
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2022-09-18 13:40:02 +0600

    collection view datasource and constraints

--- NameFaces/ViewController.swift
+++ NameFaces/ViewController.swift
@@ -13,7 +13,23 @@ class ViewController: UICollectionViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+}
 
-
+// MARK: - Collection View datasource
+extension ViewController {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "person", for: indexPath) as? PersonCell else {
+            print("Unable to dequeue Person Cell")
+            return UICollectionViewCell()
+        }
+        
+        cell.personName.text = "label"
+        
+        return cell
+    }
 }
 
